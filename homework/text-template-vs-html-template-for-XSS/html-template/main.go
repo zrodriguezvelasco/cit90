a70959b5da2105d5f70e39fea396c5ce2c6061a0
zrodriguezvelasco <0722185scccd@gmail.com>
2018-10-02 15:46:13 -0700

    demonstrate text/template vs html/template for XSS

---
+++ homework/text-template-vs-html-template-for-XSS/html-template/main.go
@@ -0,0 +1,32 @@
+package main 
+
+import (
+	"log"
+	"os"
+	"html/template"
+)
+
+type Book struct {
+	Title string
+	Author string
+	Input string
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	info := Book{
+		Title: "Hunger Games",
+		Author: "Suzanne Collins",
+		Input: `<script>alert("Yow!");</script>`,
+	}
+
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", info)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
